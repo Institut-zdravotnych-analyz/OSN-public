b3f726f8276009634a52410379be92c109b8c3cb
Oliver Velich <bielovico@gmail.com>
2025-03-20 09:16:20 +0100

    reverted paths + RS and Lucenec

--- vyhodnotenie_siete/podmienky_pre_tvorbu_siete.py
+++ vyhodnotenie_siete/podmienky_pre_tvorbu_siete.py
@@ -28,9 +28,7 @@ def _nacitaj_dojazdovu_maticu():
     Oblasť Bratislavy sa pre potreby výpočtu dojazdu považuje za 1 sídlo. Rovnako to platí aj pre Košice.
     """
 
-    dojazdova_matica = pd.read_csv(
-        "vyhodnotenie_siete/dojazdova_matica/dojazdova_matica.csv", index_col=0
-    )
+    dojazdova_matica = pd.read_csv("dojazdova_matica/dojazdova_matica.csv", index_col=0)
     dojazdova_matica.index = dojazdova_matica.index.astype("str")
     return dojazdova_matica
 
@@ -352,14 +350,14 @@ def vyhodnot_spady(siet_nemocnic, urovne_siete, poistenci_na_ZUJ, poistenci_na_o
 # Zabezpečenie minimálneho počtu lôžok
 
 MIN_POCTY_LOZOK_TYP = pd.read_excel(
-    "vyhodnotenie_siete/lozka/minimálny počet lôžok.xlsx", sheet_name="Typ"
+    "lozka/minimálny počet lôžok.xlsx", sheet_name="Typ"
 )
 # minimalne_pocty_lozok_oddelenie = pd.read_excel('minimálny počet lôžok.xlsx', sheet_name='Oddelenie', index_col=1)
 
 
 def vyhodnot_lozka(rok):
     pocty_lozok = pd.read_excel(
-        f"vyhodnotenie_siete/lozka/pocty_lozok_{rok}.xlsx",
+        f"lozka/pocty_lozok_{rok}.xlsx",
         usecols="V,Y,Z,AK",
         dtype={"CISR_ODB": "str"},
     )
